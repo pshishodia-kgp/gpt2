af36da9597507b192470445f0829a550d5d8fd18
Prashant Shishodia <pshishodia@Prashants-MacBook-Pro.local>
2024-06-24 03:12:09 +0530

    train on tinyshakespeare ~4k tok/s

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,7 +3,11 @@ import torch.nn as nn
 from transformers import GPT2LMHeadModel
 import torch.nn.functional as F
 
+# torch.set_float32_matmul_precision('medium')
 
+torch.manual_seed(424)
+device = 'mps' if torch.backends.mps.is_available() else 'cpu'
+# device = 'cpu'
 class GPTConfig:
     VOCAB_SIZE = 50257
     DIM = 768
@@ -14,16 +18,15 @@ class GPTConfig:
 class MLP(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.c_fc = nn.Linear(config.DIM, 4 * config.DIM) ## up projection
+        self.c_fc = nn.Linear(config.DIM, 4 * config.DIM, dtype=torch.bfloat16) ## up projection
         self.gelu = nn.GELU(approximate='tanh')
-        self.c_proj = nn.Linear(4 * config.DIM, config.DIM)
+        self.c_proj = nn.Linear(4 * config.DIM, config.DIM, dtype=torch.bfloat16)
 
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
-
 
 class CausalSelfAttention(nn.Module):
     def __init__(self, config):
@@ -33,8 +36,8 @@ class CausalSelfAttention(nn.Module):
 
         self.dim, self.num_heads = config.DIM, config.NUM_HEADS
         # key, query, value projections combined.
-        self.c_attn = nn.Linear(config.DIM, 3 * config.DIM)
-        self.c_proj = nn.Linear(config.DIM, config.DIM)
+        self.c_attn = nn.Linear(config.DIM, 3 * config.DIM, dtype=torch.bfloat16)
+        self.c_proj = nn.Linear(config.DIM, config.DIM, dtype=torch.bfloat16)
         
     def forward(self, x):
         qkv = self.c_attn(x)
@@ -54,9 +57,9 @@ class CausalSelfAttention(nn.Module):
 class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.ln_1 = nn.LayerNorm(config.DIM)
+        self.ln_1 = nn.LayerNorm(config.DIM, dtype=torch.bfloat16)
         self.attn = CausalSelfAttention(config)
-        self.ln_2 = nn.LayerNorm(config.DIM)
+        self.ln_2 = nn.LayerNorm(config.DIM, dtype=torch.bfloat16)
         self.mlp = MLP(config)
 
     def forward(self, x):
@@ -70,12 +73,12 @@ class GPT(nn.Module):
         super().__init__()
         self.config = config
         self.transformer = nn.ModuleDict(dict(
-            wpe = nn.Embedding(config.MAX_TOKENS, config.DIM),
-            wte = nn.Embedding(config.VOCAB_SIZE, config.DIM),
+            wpe = nn.Embedding(config.MAX_TOKENS, config.DIM, dtype=torch.bfloat16),
+            wte = nn.Embedding(config.VOCAB_SIZE, config.DIM, dtype=torch.bfloat16),
             h = nn.ModuleList([Block(config) for _ in range(config.NUM_LAYERS)]),
-            ln_f = nn.LayerNorm(config.DIM)
+            ln_f = nn.LayerNorm(config.DIM, dtype=torch.bfloat16)
         ))
-        self.lm_head =  nn.Linear(config.DIM, config.VOCAB_SIZE, bias=False)
+        self.lm_head =  nn.Linear(config.DIM, config.VOCAB_SIZE, bias=False, dtype=torch.bfloat16)
         self.lm_head.weight = self.transformer.wte.weight
 
     @classmethod
@@ -95,12 +98,13 @@ class GPT(nn.Module):
                     model.state_dict()[k].copy_(gpt2_hf.state_dict()[k])
         return model
     
-    def forward(self, inputs_BT, targets_B = None):
+    def forward(self, inputs_BT, targets_BT = None):
         B, T = inputs_BT.shape
-        assert T < self.config.MAX_TOKENS, f'{T} tokens exceed the limit of {self.config.MAX_TOKENS}'
+        assert T <= self.config.MAX_TOKENS, f'{T} tokens exceed the limit of {self.config.MAX_TOKENS}'
         
         hidden_states_BTD = self.transformer.wte(inputs_BT)
         position_embeddings_TD = self.transformer.wpe(torch.arange(0, T, device=inputs_BT.device))
+
         hidden_states_BTD = hidden_states_BTD + position_embeddings_TD
         
         for block in self.transformer.h:
@@ -108,18 +112,21 @@ class GPT(nn.Module):
         hidden_states_BTD = self.transformer.ln_f(hidden_states_BTD)
         logits_BTV = self.lm_head(hidden_states_BTD)
         loss = None
+        if targets_BT != None:
+            loss = F.cross_entropy(logits_BTV.view(-1, logits_BTV.size(-1)), targets_BT.view(-1))
         return logits_BTV, loss
     
     @torch.no_grad
     def generate_(self, inputs_BT, max_tokens = 20):
-        print(inputs_BT.shape)
+
         while inputs_BT.shape[1] < max_tokens:
             logits_BTV, _ = self(inputs_BT)
             logits_BV = logits_BTV[:,-1,:] ## Take the logits only for last element.
+
             probs_BV = logits_BV.softmax(dim=-1)
-            
             ## Top-k = 50
             probs_BK, indics_BK = probs_BV.topk(50, dim=-1)
+
             ixs = probs_BK.multinomial(1)
             next_token_B1 = indics_BK.gather(dim=1, index=ixs)
             inputs_BT = torch.cat((inputs_BT, next_token_B1), dim=1)
@@ -127,16 +134,68 @@ class GPT(nn.Module):
 
     @torch.no_grad
     def generate(self, prompt="", max_tokens=20, num_samples=5):
-        input_tokens_BT = torch.tensor(tokenizer.encode(prompt)).repeat(num_samples, 1)
+        input_tokens_BT = torch.tensor(tokenizer.encode(prompt)).repeat(num_samples, 1).to(device)
         generated_BT = self.generate_(input_tokens_BT, max_tokens=max_tokens)
         return [tokenizer.decode(x) for x in generated_BT.tolist()]
             
     
+torch.manual_seed(1337)
+
+print('Encoding...')
 ## Tokenizer
 import tiktoken
 tokenizer = tiktoken.get_encoding('gpt2')
-model = GPT.from_pretrained('gpt2')
+full_text = open('tinyshakespeare.txt').read()
+full_text_encodings = tokenizer.encode(full_text)
+print('Text encoded...')
+
+import random 
+def get_batch(batch_size=32, max_tokens = 1024):
+    start_pos = [random.randint(0, len(full_text_encodings) - max_tokens - 1) for _ in range(batch_size)]
+    xs = torch.tensor([full_text_encodings[i:i+max_tokens] for i in start_pos])
+    ys = [full_text_encodings[i+1:i+max_tokens+1] for i in start_pos]
+    # print(xs, ys)
+    return torch.tensor(xs, dtype=torch.long, device=device), torch.tensor(ys, dtype=torch.long, device=device)
+    
+# model = GPT.from_pretrained('gpt2')
 torch.manual_seed(42)
-print('\n'.join(model.generate("Hello, I'm a language model,", max_tokens=32)))
+torch.manual_seed(1337)
+
+# print('\n'.join(model.generate("Hello, I'm a language model,", max_tokens=10, num_samples=1)))
+
+model = GPT(GPTConfig()).to(device)
+# model = torch.compile(model)
+# print('Compiled.')
+import time
+
+BATCH_SIZE = 4
+MAX_TOKENS = 1024
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
+for epoch in range(501):
+    if epoch == 100:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.005
+    if epoch == 200:
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = 0.001
+
+    start_time = time.time()
+    
+    xs, ys = get_batch(BATCH_SIZE, MAX_TOKENS)
+    _, loss = model(xs, ys)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    end_time = time.time()
+    
+    print(f'Epoch : {epoch} |Loss : {loss.item()} | Tokens/sec : {BATCH_SIZE * MAX_TOKENS / (end_time - start_time)}')
+    if epoch % 50 == 0:
+        print('\n'.join(model.generate("\n", max_tokens=20, num_samples=5)))
+
+        
+# import code
+# code.interact(local=locals())
+
 
 
